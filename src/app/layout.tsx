500a2a8dcd5e0fc33d7da1fa54537961ed4668e7
miyuruhimansa1116-rgb <miyuruhimansa1116@gmail.com>
2026-09-23 16:58:20 +0530

    layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Sampath Auto Parts",
+  title: "Auto Electrical POS System",
   description: "POS and Inventory Management System",
 };
 
